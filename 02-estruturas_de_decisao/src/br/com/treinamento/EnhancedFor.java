da994131f4269205c524c7cf2c269dd9f76a2036
RiqueYam <yugo.yamane@yahoo.com>
2024-03-14 11:37:33 -0300

    EnhancedFor finalizado

--- 02-estruturas_de_decisao/src/br/com/treinamento/EnhancedFor.java
+++ 02-estruturas_de_decisao/src/br/com/treinamento/EnhancedFor.java
@@ -3,8 +3,15 @@ package br.com.treinamento;
 public class EnhancedFor {
 
 	public static void main(String[] args) {
-
 		
+		int[] arraylist = {10, 20, 30, 40, 50, 60, 70};
+		int contador = 0;
+		System.out.println("Lista de números: ");
+		for(int array : arraylist) {
+			System.out.println(array);
+			contador++;
+		}
+		System.out.println("Quantidade de registros: " + contador);
 	}
 
 }
